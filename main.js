ecc6b37148ce99367499b1aa72915dbbd755db97
ArnavRanjan <83539005+ArnavRanjan@users.noreply.github.com>
2021-12-06 19:48:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+ function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300,400);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function save_image(){
+    save("your_image.png");
+}
